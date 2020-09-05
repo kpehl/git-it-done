177bb581455476ac7be02161e2a7558ae27e6cd7
kpehl <30782087+kpehl@users.noreply.github.com>
2020-09-05 10:21:55 -0400

    modified function to take a string input for a user name and return that user's repo list in the console

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,10 +1,12 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+// A function to retrieve repo information from GitHub
+var getUserRepos = function(user) {
+    // Format the GitHub API URL for the fetch function
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // Make a request to the URL
+    fetch(apiUrl).then(function(response) {
         response.json().then(function(data) {
             console.log(data);
         });
 });
 };
-
-
-getUserRepos();
