199bf6545538e83815bcb8cb6865683996805dc8
kpehl <30782087+kpehl@users.noreply.github.com>
2020-09-05 13:29:37 -0400

    added html form to search for a user name, added to homepage.js to handle form input and call function to retrieve data from GitHub API

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -10,3 +10,32 @@ var getUserRepos = function(user) {
         });
 });
 };
+
+// Variables for user name search form
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+// Form Submit Handler Function
+var formSubmitHandler = function(event) {
+    // prevent the submit default action of submitting the information to a URL
+    event.preventDefault();
+    // get value from the input element
+    var username = nameInputEl.value.trim();
+    // call the function to retrieve the repo list of the user and reset the input value
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    // alert if a user name was not entered
+    } else {
+        alert("Please enter a GitHub username");
+    }
+};
+
+// Function to display repos on the html page
+var displayRepos = function(repos, searchTerm) {
+    console.log(repos);
+    console.log(searchTerm);
+};
+
+// Event listener for form submittal
+userFormEl.addEventListener("submit", formSubmitHandler);
