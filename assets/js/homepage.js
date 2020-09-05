51cfbc7f8868548c7f40f66cd3b0663f3583daff
kpehl <30782087+kpehl@users.noreply.github.com>
2020-09-05 13:50:53 -0400

    added function to display the list of repositories for the entered user name on the html page

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -6,7 +6,7 @@ var getUserRepos = function(user) {
     // Make a request to the URL
     fetch(apiUrl).then(function(response) {
         response.json().then(function(data) {
-            console.log(data);
+            displayRepos(data, user);
         });
 });
 };
@@ -31,10 +31,31 @@ var formSubmitHandler = function(event) {
     }
 };
 
+// Variables for displaying the repo list in the DOM
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+
 // Function to display repos on the html page
 var displayRepos = function(repos, searchTerm) {
-    console.log(repos);
-    console.log(searchTerm);
+    // clear out old content
+    repoContainerEl.textContent = "";
+    // add the user name that was entered to the heading
+    repoSearchTerm.textContent = searchTerm;
+    // loop over the repos
+    for (var i=0; i < repos.length; i++) {
+        // format the repo name from the JSON data object
+        var repoName = repos[i].owner.login + "/" + repos[i].name;
+        // create a container for each repo
+        var repoEl = document.createElement("div");
+        repoEl.classList = "list-item flex-row justify-space-between align-center";
+        // create a span element to hold the repository name and add the name from imported data
+        var titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
+        // append the information to the container element
+        repoEl.appendChild(titleEl);
+        // append the container to the DOM
+        repoContainerEl.appendChild(repoEl);
+    };
 };
 
 // Event listener for form submittal
